71d5bd15be87181c8f9ff99af0475d8e7db1a0c6
rho1510 <56241084+rho1510@users.noreply.github.com>
2021-06-21 21:32:26 +0900

    devB version

--- MyGit Java/src/A.java
+++ MyGit Java/src/A.java
@@ -1,6 +1,7 @@
 
 public class A {
 	public static void main(String args[]) {
-		System.out.println("A first version");
+		System.out.print("A first version");
+		System.out.print(" + devB version");
 	}
 }
--- MyGit Java/src/B.java
+++ MyGit Java/src/B.java
@@ -1,6 +1,7 @@
 
 public class B {
 	public static void main(String args[]) {
-		System.out.println("B first version");
+		System.out.print("B first version");
+		System.out.print(" + devB version");
 	}
 }
