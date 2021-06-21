c9690689c30e5421eaf8e47cbf6452409ced3d41
rho1510 <56241084+rho1510@users.noreply.github.com>
2021-06-21 21:33:52 +0900

    devC version

--- MyGit Java/src/C.java
+++ MyGit Java/src/C.java
@@ -1,6 +1,7 @@
 
 public class C {
 	public static void main(String args[]) {
-		System.out.println("C first version");
+		System.out.print("C first version");
+		System.out.print(" + devC version");
 	}
 }
